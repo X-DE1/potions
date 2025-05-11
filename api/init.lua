e0603964cfcb4a0edb86ac75ce00096f8167cd19
X-DE <101435795+X-DE1@users.noreply.github.com>
2025-05-11 12:30:03 -0600

    Update init.lua

--- api/init.lua
+++ api/init.lua
@@ -1,7 +1,8 @@
 -- Copyright (C) 2024 Stakbox
+-- Copyright (C) 2025 X-DE
 -- This file is licensed under the GNU General Public License, version 3.0 or later
 -- It is distributed WITHOUT ANY WARRANTY
--- See LICENSE.txt for more details
+-- See LICENSE.md for more details
  
 local path = core.get_modpath("potions_api")
 
