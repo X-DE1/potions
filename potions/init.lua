195a257066eda0e465e784df70e753ffc8060788
X-DE <101435795+X-DE1@users.noreply.github.com>
2025-05-11 12:29:26 -0600

    Update init.lua

--- potions/init.lua
+++ potions/init.lua
@@ -2,7 +2,7 @@
 -- Copyright (C) 2025 X-DE
 -- This file is licensed under the GNU General Public License, version 3.0 or later
 -- It is distributed WITHOUT ANY WARRANTY
--- See LICENSE.txt for more details
+-- See LICENSE.md for more details
  
 local path = core.get_modpath("potions_default")
 
