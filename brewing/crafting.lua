869dc6d474896dfd06ccda0c7ff0f8c3a14235ef
X-DE <101435795+X-DE1@users.noreply.github.com>
2025-05-11 12:16:52 -0600

    Update crafting.lua

--- brewing/crafting.lua
+++ brewing/crafting.lua
@@ -1,4 +1,5 @@
 -- Copyright (C) 2024 Stakbox
+-- Copyright (C) 2025 X-DE
 -- This file is licensed under the GNU General Public License, version 3.0 or later
 -- It is distributed WITHOUT ANY WARRANTY
 -- See LICENSE.txt for more details
